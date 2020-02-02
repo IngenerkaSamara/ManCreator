11d642b8e27fe84f9d83ff0d57ed10259a5d0684
Mikhail Abramov <beavisabra@yandex.ru>
2020-02-02 17:48:26 +0400

    Деградировал

--- main.cpp
+++ main.cpp
@@ -1,5 +1,14 @@
 #include "TXLib.h"
 
+void drawButton(int x, int y, const char* text)
+{
+    txSetColor(TX_BLACK, 4);
+    txSetFillColor(TX_YELLOW);
+    txSelectFont("Comic Sans MS", 40);
+    txRectangle(x, y, x + 200, y + 100);
+    txDrawText (x, y, x + 200, y + 100, text);
+}
+
 int main()
 {
     txCreateWindow(1200,800);
@@ -9,10 +18,15 @@ int main()
         txBegin();
         txSetFillColor(TX_WHITE);
         txClear();
-     
-	txSetColor(TX_BLACK);
+
+        txSetColor(TX_BLACK);
         txSelectFont("Comic Sans MS", 60);
         txTextOut(200,700, "1");
+
+
+        drawButton(100, 0, "Уши");
+        drawButton(300, 0, "Прически");
+
 
         txSleep(10);
         txEnd();
