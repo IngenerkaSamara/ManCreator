03b0c8f529ebed68a58829f27a2e2f456d97d794
Mikhail Abramov <beavisabra@yandex.ru>
2020-02-09 13:11:03 +0400

    Передвижение

--- Picture.cpp
+++ Picture.cpp
@@ -1,46 +1,62 @@
 #include "TXLib.h"
+#include <iostream>
+#include <conio.h>
+#include <fstream>
+#include<stdio.h>
+#include<stdlib.h>
+
+using namespace std;
 
 struct Picture
 {
     int x;
     int y;
+    string adress;
     HDC image;
-    int width;
-    int height;
+    int src_width;
+    int src_height;
     bool visible;
     string category;
+    int width;
+    int height;
 };
+
+int getHeight(string adress)
+{
+    FILE *f1 = fopen(adress.c_str(), "rb");
+
+    unsigned char headerinfo[54];
+    fread(headerinfo, sizeof(unsigned char), 54, f1);
+
+    int height = *(int*)&headerinfo[22];
+
+    return height;
+}
+
+int getWidth(string adress)
+{
+    FILE *f1 = fopen(adress.c_str(), "rb");
+
+    unsigned char headerinfo[54];
+    fread(headerinfo, sizeof(unsigned char), 54, f1);
+
+    int width = *(int*)&headerinfo[18];
+
+    return width;
+}
 
 void drawPicture(Picture pct)
 {
     if (pct.visible)
     {
-        Win32::TransparentBlt(txDC(), pct.x, pct.y, 100, 100, pct.image, 0, 0, pct.width, pct.height, TX_WHITE);
+        Win32::TransparentBlt(txDC(), pct.x, pct.y, pct.width, pct.height, pct.image, 0, 0, pct.src_width, pct.src_height, TX_WHITE);
     }
 }
 
-void drawPicture2(Picture pct)
-{
-    if (pct.visible)
-    {
-        Win32::TransparentBlt(txDC(), pct.x, pct.y, pct.width, pct.height, pct.image, 0, 0, pct.width, pct.height, TX_WHITE);
-    }
-}
-
-void drawAllLeftPictures(Picture leftPictures[], const int COUNT_PICS)
+void drawAllPictures(Picture leftPictures[], const int COUNT_PICS)
 {
     for (int nPict = 0; nPict < COUNT_PICS; nPict++)
     {
         drawPicture(leftPictures[nPict]);
     }
 }
-
-void drawAllCenterPictures(Picture centerPictures[], const int COUNT_PICS)
-{
-    for (int nPict = 0; nPict < COUNT_PICS; nPict++)
-    {
-        drawPicture2(centerPictures[nPict]);
-    }
-}
-
-
--- main.cpp
+++ main.cpp
@@ -11,28 +11,45 @@ int main()
 
     const int COUNT_PICS = 9;
     Picture leftPictures[COUNT_PICS];
-    leftPictures[0] = {0, 100, txLoadImage("Pictures/Нос/Нос1.bmp"), 126, 104, false, "Нос"};
-    leftPictures[1] = {0, 250, txLoadImage("Pictures/Нос/Нос2.bmp"), 100, 100, false, "Нос"};
-    leftPictures[2] = {0, 400, txLoadImage("Pictures/Нос/Нос3.bmp"), 100, 100, false, "Нос"};
-    leftPictures[3] = {0, 100, txLoadImage("Pictures/Волосы/Волосы1.bmp"), 400, 283, false, "Волосы"};
-    leftPictures[4] = {0, 250, txLoadImage("Pictures/Волосы/Волосы2.bmp"), 400, 309, false, "Волосы"};
-    leftPictures[5] = {0, 400, txLoadImage("Pictures/Волосы/Волосы3.bmp"), 448, 558, false, "Волосы"};
-    leftPictures[6] = {0, 100, txLoadImage("Pictures/Глаза/Глаза1.bmp"), 250, 125, false, "Глаза"};
-    leftPictures[7] = {0, 250, txLoadImage("Pictures/Глаза/Глаза2.bmp"), 250, 134, false, "Глаза"};
-    leftPictures[8] = {0, 400, txLoadImage("Pictures/Глаза/Глаза3.bmp"), 250, 100, false, "Глаза"};
+    leftPictures[0] = {0, 100, "Pictures/Волосы/Волосы1.bmp", txLoadImage("Pictures/Волосы/Волосы1.bmp"), 0, 0, false, "Волосы"};
+    leftPictures[1] = {0, 250, "Pictures/Волосы/Волосы2.bmp", txLoadImage("Pictures/Волосы/Волосы2.bmp"), 0 , 0, false, "Волосы"};
+    leftPictures[2] = {0, 400, "Pictures/Волосы/Волосы3.bmp", txLoadImage("Pictures/Волосы/Волосы3.bmp"), 448, 558, false, "Волосы"};
+    leftPictures[3] = {0, 100, "Pictures/Нос/Нос1.bmp", txLoadImage("Pictures/Нос/Нос1.bmp"), 126, 104, false, "Нос"};
+    leftPictures[4] = {0, 250, "Pictures/Нос/Нос2.bmp", txLoadImage("Pictures/Нос/Нос2.bmp"), 100, 100, false, "Нос"};
+    leftPictures[5] = {0, 400, "Pictures/Нос/Нос3.bmp", txLoadImage("Pictures/Нос/Нос3.bmp"), 100, 100, false, "Нос"};
+    leftPictures[6] = {0, 100, "Pictures/Глаза/Глаза1.bmp", txLoadImage("Pictures/Глаза/Глаза1.bmp"), 250, 125, false, "Глаза"};
+    leftPictures[7] = {0, 250, "Pictures/Глаза/Глаза2.bmp", txLoadImage("Pictures/Глаза/Глаза2.bmp"), 250, 134, false, "Глаза"};
+    leftPictures[8] = {0, 400, "Pictures/Глаза/Глаза3.bmp", txLoadImage("Pictures/Глаза/Глаза3.bmp"), 250, 100, false, "Глаза"};
 
 
 
     Picture centerPictures[COUNT_PICS];
-    centerPictures[0] = {350, 380, txLoadImage("Pictures/Нос/Нос1.bmp"), 126, 104, false, "Нос"};
-    centerPictures[1] = {350, 380, txLoadImage("Pictures/Нос/Нос2.bmp"), 100, 100, false, "Нос"};
-    centerPictures[2] = {350, 380, txLoadImage("Pictures/Нос/Нос3.bmp"), 100, 100, false, "Нос"};
-    centerPictures[3] = {200, 100, txLoadImage("Pictures/Волосы/Волосы1.bmp"), 400, 283, false, "Волосы"};
-    centerPictures[4] = {200, 100, txLoadImage("Pictures/Волосы/Волосы2.bmp"), 400, 309, false, "Волосы"};
-    centerPictures[5] = {200, 100, txLoadImage("Pictures/Волосы/Волосы3.bmp"), 448, 558, false, "Волосы"};
-    centerPictures[6] = {300, 270, txLoadImage("Pictures/Глаза/Глаза1.bmp"), 250, 125, false, "Глаза"};
-    centerPictures[7] = {300, 270, txLoadImage("Pictures/Глаза/Глаза2.bmp"), 250, 134, false, "Глаза"};
-    centerPictures[8] = {300, 270, txLoadImage("Pictures/Глаза/Глаза3.bmp"), 250, 100, false, "Глаза"};
+    centerPictures[0] = {200, 100};
+    centerPictures[1] = {200, 100};
+    centerPictures[2] = {200, 100};
+    centerPictures[3] = {350, 380};
+    centerPictures[4] = {350, 380};
+    centerPictures[5] = {350, 380};
+    centerPictures[6] = {300, 270};
+    centerPictures[7] = {300, 270};
+    centerPictures[8] = {300, 270};
+
+    //Ширина и высота
+    for (int nPict = 0; nPict < COUNT_PICS; nPict++)
+    {
+        leftPictures[nPict].width = 100;
+        leftPictures[nPict].height = 100;
+        leftPictures[nPict].src_width = getWidth(leftPictures[nPict].adress);
+        leftPictures[nPict].src_height = getHeight(leftPictures[nPict].adress);
+        centerPictures[nPict].category = leftPictures[nPict].category;
+        centerPictures[nPict].visible = false;
+        centerPictures[nPict].adress = leftPictures[nPict].adress;
+        centerPictures[nPict].image = leftPictures[nPict].image;
+        centerPictures[nPict].src_width = leftPictures[nPict].src_width;
+        centerPictures[nPict].src_height = leftPictures[nPict].src_height;
+        centerPictures[nPict].width = centerPictures[nPict].src_width;
+        centerPictures[nPict].height = centerPictures[nPict].src_height;
+    }
 
 
 
@@ -41,6 +58,11 @@ int main()
     btn[1] = {300, 0, "Носы", "Нос"};
     btn[2] = {500, 0, "Прически", "Волосы"};
     btn[3] = {700, 0, "Глаза", "Глаза"};
+
+    int nomer_vybora = -1;
+    bool mouseDown = false;
+    int currentX = 0;
+    int currentY = 0;
 
 
     while (!GetAsyncKeyState(VK_ESCAPE))
@@ -57,8 +79,68 @@ int main()
         }
 
 
-        drawAllLeftPictures(leftPictures, COUNT_PICS);
-        drawAllCenterPictures(centerPictures, COUNT_PICS);
+        drawAllPictures(leftPictures, COUNT_PICS);
+        drawAllPictures(centerPictures, COUNT_PICS);
+
+
+        //Выбор картинки в центре
+        for (int nPict = 0; nPict < COUNT_PICS; nPict++)
+        {
+            if (txMouseButtons() == 1 &&
+                centerPictures[nPict].visible &&
+                txMouseX() >= centerPictures[nPict].x &&
+                txMouseX() <= centerPictures[nPict].x + centerPictures[nPict].width &&
+                txMouseY() >= centerPictures[nPict].y &&
+                txMouseY() <= centerPictures[nPict].y + centerPictures[nPict].height)
+            {
+                nomer_vybora = nPict;
+                currentX = centerPictures[nPict].x - txMouseX();
+                currentY = centerPictures[nPict].y - txMouseY();
+                mouseDown = false;
+            }
+        }
+
+        //Движение
+        if (nomer_vybora >= 0)
+        {
+            if (txMouseButtons() == 1 && !mouseDown)
+            {
+                centerPictures[nomer_vybora].x = txMouseX() + currentX;
+                centerPictures[nomer_vybora].y = txMouseY() + currentY;
+            }
+            else if (txMouseButtons() != 1)
+            {
+                mouseDown = true;
+            }
+
+            if (GetAsyncKeyState(VK_LEFT))
+            {
+                centerPictures[nomer_vybora].x -= 3;
+            }
+            if (GetAsyncKeyState(VK_RIGHT))
+            {
+                centerPictures[nomer_vybora].x += 3;
+            }
+            if (GetAsyncKeyState(VK_UP))
+            {
+                centerPictures[nomer_vybora].y -= 3;
+            }
+            if (GetAsyncKeyState(VK_DOWN))
+            {
+                centerPictures[nomer_vybora].y += 3;
+            }
+
+            if (GetAsyncKeyState(VK_OEM_PLUS) && centerPictures[nomer_vybora].width < 600)
+            {
+                centerPictures[nomer_vybora].width = centerPictures[nomer_vybora].width * 1.02;
+                centerPictures[nomer_vybora].height = centerPictures[nomer_vybora].height * 1.02;
+            }
+            if (GetAsyncKeyState(VK_OEM_MINUS) && centerPictures[nomer_vybora].width > 20)
+            {
+                centerPictures[nomer_vybora].width = centerPictures[nomer_vybora].width * 0.98;
+                centerPictures[nomer_vybora].height = centerPictures[nomer_vybora].height * 0.98;
+            }
+        }
 
 
         for (int nPict = 0; nPict < COUNT_PICS; nPict++)
@@ -106,6 +188,13 @@ int main()
 
         txSleep(10);
         txEnd();
+    }
+
+
+    for (int nPict = 0; nPict < COUNT_PICS; nPict++)
+    {
+        txDeleteDC(centerPictures[nPict].image);
+        txDeleteDC(leftPictures[nPict].image);
     }
 
 
